366b9bec4cdc8fa90f54f2ffa35ac04b372bf1b7
Yannis Rizos <yannis@walkinthepark.gr>
2013-12-05 14:28:18 +0200

    small bug fix

--- ajaxForm/ajaxForm.js
+++ ajaxForm/ajaxForm.js
@@ -3,46 +3,47 @@
     "use strict";
 
     $.fn.ajaxForm = function (options) {
-        options = $.extend({}, $.fn.ajaxForm.defaults, options);
+        options = $.extend ({}, $.fn.ajaxForm.defaults, options);
 
-        function callHandler(name, form, args) {
+        function callHandler (name, form, args) {
             if (typeof options[name] === "function") {
-                options[name].apply(form, [].slice.call(args, 0));
+                options[name].apply (form, [].slice.call (args, 0));
             }
         }
 
-        return this.filter("form").each(function () {
+        return this.filter ("form").each (function () {
             var settings, dataType, form = this;
 
-            settings = $.extend({}, {
-                url: $(this).prop("action"),
-                type: $(this).prop("method"),
-                data: $(this).serialize()
+            settings = $.extend ({}, {
+                url: $ (this).prop ("action"),
+                type: $ (this).prop ("method")
             }, options);
 
-            dataType = $(this).attr("data-dataType");
+            dataType = $ (this).attr ("data-dataType");
             if (typeof dataType === "string") {
                 settings.dataType = dataType;
             }
 
             settings.beforeSend = function () {
-                $(this).addClass("ajaxForm-loading");
-                callHandler("beforeSend", form, arguments);
+                $ (this).addClass ("ajaxForm-loading");
+                callHandler ("beforeSend", form, arguments);
             };
 
             settings.complete = function () {
-                $(this).removeClass("ajaxForm-loading");
-                callHandler("complete", form, arguments);
+                $ (this).removeClass ("ajaxForm-loading");
+                callHandler ("complete", form, arguments);
             };
 
-            $(this).on("submit", function (event) {
-                event.preventDefault();
+            $ (this).on ("submit", function (event) {
+                event.preventDefault ();
 
-                $.ajax(settings);
+                settings.data = $ (this).serialize ();
+
+                $.ajax (settings);
             });
         });
     };
 
     $.fn.ajaxForm.defaults = {};
 
-})(jQuery);
+}) (jQuery);
